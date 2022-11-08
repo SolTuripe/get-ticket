02e3ccccd0b1959235bc7f1f98b86f83a6e6e086
Sol Turipe <solcis.bio@gmail.com>
2022-11-08 13:25:24 +0100

    refactoring

--- service.js
+++ service.js
@@ -3,22 +3,23 @@ import { data } from "./data/data.js";
 function getProductsTotal(products) {
   let total = 0;
   products.forEach((product) => {
-    total = total + product.quantity * product.unitPrice;
+    total += product.quantity * product.unitPrice;
   });
   return total;
 }
 
 function printInvoiceLines(products) {
   const table = document.querySelector("#lines");
-  products.forEach((product) => {
+  products.map((product) => {
     table.innerHTML += `
-            <tr>
-                <td>${product.quantity}</td>
-                <td>${product.product}</td>
-                <td>${product.unitPrice}</td>
-                <td>${product.unitPrice * product.quantity}</td>
-            </tr>
-        `;
+    <tr>
+    <td>${product.quantity}</td>
+    <td>${product.product}</td>
+    <td>${product.type}</td>
+    <td>${product.unitPrice}</td>
+    <td>${product.quantity * product.unitPrice}</td>
+    </tr>
+    `;
   });
   table.innerHTML += `<tr><td>Total: ${getProductsTotal(products)}</td></tr>`;
 }
